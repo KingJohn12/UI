d02c89fdcb09533dff9bc16539f5eb2e993e5eb7
Alec Joseph <blackdragon98@live.com>
2026-06-25 15:34:28 -0400

    New update

---
+++ styles.js
@@ -0,0 +1,21 @@
+import {StyleSheet} from "react-native";
+
+
+export default function Stylesheet.create {(
+container: {flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20,
+}
+
+title: {
+    fontSize: 28px;
+    marginBottom: 20,
+
+}
+
+
+
+
+
+)}
